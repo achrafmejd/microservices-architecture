e2fc812d88f0d0945f9d18700046e41a2ba0fbd1
theM3JD <achraf.mejd.dev@gmail.com>
2022-11-13 15:14:46 +0100

    Microservice 101

---
+++ service-company/service-company/src/main/java/org/sid/web/CompanyRestService.java
@@ -0,0 +1,41 @@
+package org.sid.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RefreshScope
+@RestController
+public class CompanyRestService {
+    
+    @Value("${xParam}")
+    private int xParam;
+    @Value("${yParam}")
+    private int yParam;
+    @Value("${me}")
+    private String me;
+
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World !";
+    }
+    @GetMapping("/myconfig")
+    public Map<String, Object> myConfig(){
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("xParam", xParam);
+        params.put("yParam", yParam);
+        params.put("me", me);
+
+        params.put("threadName", Thread.currentThread().getName());
+        return params;
+    }
+
+}
